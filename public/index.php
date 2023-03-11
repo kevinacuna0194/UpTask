eb08b9768bf821167ab2011b56ae60fea3f321ee
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-11 14:31:21 -0300

    Primeros Pasos con el Routing

--- public/index.php
+++ public/index.php
@@ -3,10 +3,28 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\LoginController;
 $router = new Router();
 
+$router->get('/', [LoginController::class, 'login']);
+$router->post('/', [LoginController::class, 'login']);
+$router->get('/logout', [LoginController::class, 'logout']);
 
+/** Crear Cuenta */
+$router->get('/crear', [LoginController::class, 'crear']);
+$router->post('/crear', [LoginController::class, 'crear']);
 
+/** Formulario de Olvide mi Password */
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
 
-// Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
+/** Colocar el Nuevo Password */
+$router->get('/reestablecer', [LoginController::class, 'reestablecer']);
+$router->post('/reestablecer', [LoginController::class, 'reestablecer']);
+
+/** Confirmación de Cuenta */
+$router->get('/mensaje', [LoginController::class, 'mensaje']);
+$router->get('/confirmar', [LoginController::class, 'confirmar']);
+
+/** Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador */
 $router->comprobarRutas();
