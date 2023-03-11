b50bf864335aee75d22869be820516eeec4959e7
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-11 17:38:44 -0300

    Creando el Formulario de Login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,38 @@
-<h1>UpTask</h1>
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p>Crea y Administra tus Proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form class="formulario" action="/" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input 
+                    type="email" 
+                    id="email" 
+                    placeholder="Tu Email" 
+                    name="email"
+                />
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    placeholder="Tu Password" 
+                    name="password"
+                />
+            </div>
+
+            <input type="submit" class='boton' value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">¿Aún no tienes una cuenta? Crea una</a>
+            <a href="/olvide">¿Olvidaste tu Password?</a>
+        </div>
+
+    </div> <!-- contenedor-sm -->
+</div> <!-- contenedor -->
