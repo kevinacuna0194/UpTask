69ff0816fcd44b0a3d15f497d0b4015375b529ba
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-12 15:34:45 -0300

    Creando la vista para agregar el nuevo password

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -44,12 +44,15 @@ class LoginController
         ]);
     }
 
-    public static function reestablecer()
+    public static function reestablecer(Router $router)
     {
-        echo "Desde Reestablecer...";
-
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
+
+        /** Render a la vista */
+        $router->render('auth/reestablecer', [
+            'titulo' => 'Reestablecer Password'
+        ]);
     }
 
     public static function mensaje()
