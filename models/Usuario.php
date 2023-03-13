b730fade42e0292a60711a8e45046e2a7e6c1d44
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-03-13 18:04:15 -0300

    Verificando que los Passwords sean iguales

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 
 namespace Model;
 
-class Usuario extends ActiveRecord {
+class Usuario extends ActiveRecord
+{
     protected static $tabla = 'usuarios';
     protected static $columnasDB = ['id', 'nombre', 'email', 'password', 'token', 'confirmado'];
 
@@ -10,6 +11,7 @@ class Usuario extends ActiveRecord {
     public $nombre;
     public $email;
     public $password;
+    public $password2;
     public $token;
     public $confirmado;
 
@@ -19,18 +21,32 @@ class Usuario extends ActiveRecord {
         $this->nombre = $args['nombre'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
         $this->token = $args['token'] ?? '';
         $this->confirmado = $args['confirmado'] ?? '';
     }
 
     /** Validación para cuentas nuevas */
-    public function validarNuevaCuenta() {
-        if(!$this->nombre) {
+    public function validarNuevaCuenta()
+    {
+        if (!$this->nombre) {
             self::$alertas['error'][] = 'El Nombre del Usuario es Obligatorio';
         }
 
-        if(!$this->email) {
+        if (!$this->email) {
             self::$alertas['error'][] = 'El Email del Usuario es Obligatorio';
+        }
+
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password no Puede Estar Vacio';
+        }
+
+        if (strlen(!$this->password) < 5) {
+            self::$alertas['error'][] = 'El Password Debe Contener al Menos 5 Caracteres';
+        }
+
+        if ($this->password !== $this->password2) {
+            self::$alertas['error'][] = 'Los Passwords son Diferentes';
         }
 
         return self::$alertas;
