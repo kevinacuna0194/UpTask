6ef113f31bbccad6b69551c1b14bdc7520853c53
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2023-05-03 14:03:43 -0300

    Listando Todos los Proyectos del Usuario

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,6 +1,17 @@
-
 <?php include_once __DIR__ . '/header-dashboard.php'; ?>
 
-    <?php debuguear($proyectos); ?>
+<?php if (count($proyectos) === 0) { ?>
+    <p class="no-proyectos">No hay Proyectos Aún <a href="/crear-proyecto">Comienza Creando Uno</a></p>
+<?php } else { ?>
+    <ul class="listado-proyectos">
+        <?php foreach ($proyectos as $proyecto) { ?>
+            <li class="proyecto">
+                <a href="/proyectos?url<?php echo $proyecto->url; ?>">
+                    <?php echo $proyecto->proyecto; ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+<?php } ?>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
